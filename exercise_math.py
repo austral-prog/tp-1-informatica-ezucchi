c350991d0a676333a2cdc48f1b1a9b89caf80f8b
Eugenio Zucchi <ez.zucchieugenio@gmail.com>
2025-03-21 10:42:00 -0300

    First hand in

--- exercise_math.py
+++ exercise_math.py
@@ -1,3 +1,13 @@
 def math():
     a = 57
     b = 7
+    p1 = a + b
+    p2 = a - b
+    p3 = a * b
+    p4 = p1 / 2
+    p5 = int(a / b)
+    p6 = a % b
+    p7 = a / b
+    print(p1, p2, p3, p4, p5, p6, p7)
+
+math()
